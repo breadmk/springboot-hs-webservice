325e8e9cc70cd433e12f0c7414e6f6eeb659a126
breadmk <minvita9999@naver.com>
2021-11-10 00:13:56 +0900

    TDD Test

---
+++ src/main/java/com/company/hs/web/HelloController.java
@@ -0,0 +1,18 @@
+package com.company.hs.web;
+
+import com.company.hs.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
+    }
+}
