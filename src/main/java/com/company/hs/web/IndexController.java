8b7adb25a68e075ad112f1af7eedb124b8bc46ac
breadmk <minvita9999@naver.com>
2021-11-12 18:18:57 +0900

    oauth 시작

--- src/main/java/com/company/hs/web/IndexController.java
+++ src/main/java/com/company/hs/web/IndexController.java
@@ -1,16 +1,27 @@
 package com.company.hs.web;
 
+import com.company.hs.service.posts.PostsService;
+import com.company.hs.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.h2.engine.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 // mustache 기본 템플릿은 src/main/resources/templates 로 자동 지정
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
     
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
@@ -18,4 +29,15 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+    }
+
 }
