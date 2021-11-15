f6642b73cc3835d5ec82f15d734bd1af1d96eae2
Kang_mingyu <mgk1058@gmail.com>
2021-11-15 11:41:28 +0900

    구글 연동 Class 생성

---
+++ src/main/java/com/company/hs/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.company.hs.config.auth.dto;
+
+import com.company.hs.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+}
---
+++ src/main/java/com/company/hs/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.company.hs.domain.user;
+
+import com.company.hs.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) //Enum 타입 기본 저장은 int || 무슨 내용인지 모르기 때문에 String으로 선언.
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+}
