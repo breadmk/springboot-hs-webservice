bf76a8bd466cc79ae6930d5c686602554796a0c1
breadmk <minvita9999@naver.com>
2021-11-18 10:04:12 +0900

    자동배포 TEST

---
+++ src/main/java/com/company/hs/web/dto/TestController.java
@@ -0,0 +1,12 @@
+package com.company.hs.web.dto;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    public String test(){
+        return "test";
+    }
+}
